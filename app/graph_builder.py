8daa2ee7a4577ae3c777e9edbceaefeff082e9e2
Bryan Sky <benedictlindner@outlook.com>
2020-09-20 00:38:24 +0200

    combine single steps to pipeline

--- app/graph_builder.py
+++ app/graph_builder.py
@@ -1,12 +1,21 @@
+from app.object_extraction import extract_object_with_position
+from app.line_extraction import extract_lines
+import json
 
-def build_graph(objects, lines):
+
+def build_graph(image_path):
     """
-
-    :param objects:
-    :param lines:
+    :param image:
     :return:
     """
-    # TODO: implement
+    objects = extract_object_with_position(image_path)
+    lines = extract_lines(image_path, objects)
+    graph = Graph()
+    graph.build_from_data(objects, lines)
+    json_string = graph.to_json()
+    filename = image_path.replace('.png', '.json')
+    with open(filename, 'w') as f:
+        json.dump(json_string, f)
     return
 
 
@@ -18,3 +27,108 @@ def visualize_graph(graph):
     """
     # TODO: implement
     return
+
+
+class Graph(object):
+
+    def __init__(self):
+        self.nodes = []
+        self.lines = []
+        self.node_counter = 0
+        self.edge_counter = 0
+
+    def add_node(self, node):
+        self.nodes.append(node)
+
+    def add_line(self, line):
+        for node in self.nodes:
+            if node.has_line_connection(line):
+                if line.start is None:
+                    line.start = node.id
+                elif line.end is None:
+                    line.end = node.id
+                else:
+                    raise("Line should not have three endings")
+
+    def build_from_data(self, items, lines):
+        for item in items:
+            node = Node(self.node_counter, item['label'])
+            self.node_counter += 1
+            self.add_node(node)
+        for line in lines:
+            edge = Edge(self.edge_counter)
+            x1, y1, x2, y2 = line
+            edge.set_points([x1, y1], [x2, y2])
+            self.edge_counter += 1
+            self.add_line(edge)
+
+    def to_json(self):
+        data = {
+            'nodes': self.nodes,
+            'edges': self.lines
+        }
+        return data
+
+
+class Node(object):
+
+    def __init__(self, type, id, matching_threshold=10):
+        self.id = id
+        self.type = type
+        self.bounding_box = []
+        self.data = {}
+        self.matching_threshold = matching_threshold
+
+    def set_bounding_box(self, bounding_box):
+        self.bounding_box = bounding_box
+
+    def get_bounding_box(self):
+        return self.bounding_box
+
+    def get_type(self):
+        return self.type
+
+    def set_type(self, type):
+        self.type = type
+
+    def build_data_object(self):
+        self.data = {
+            'data': {
+                'id': self.id,
+                'label': self.type
+            }
+        }
+        return self.data
+
+    def has_line_connection(self, line):
+        x_min, y_min, x_max, y_max = self.bounding_box
+        thresholded_bounding_box = [x_min - self.matching_threshold, y_min - self.matching_threshold,
+                                    x_max + self.matching_threshold, y_max + self.matching_threshold]
+        x_min, y_min, x_max, y_max = thresholded_bounding_box
+        x1, y1, x2, y2 = line
+        return (x_min <= x1 <= x_max and y_min <= y1 <= y_max) or (x_min <= x2 <= x_max and y_min <= y2 <= y_max)
+
+
+class Edge(object):
+
+    def __init__(self, id, source=None, target=None):
+        self.id = id
+        self.source = source
+        self.target = target
+        self.start = None
+        self.end = None
+
+    def build_data_object(self):
+        obj = {
+            'data': {
+                'id': self.id,
+                'source': self.source,
+                'target': self.target
+            }
+        }
+        return obj
+
+    def set_points(self, start, end):
+        self.start = start
+        self.end = end
+
--- app/line_extraction.py
+++ app/line_extraction.py
@@ -1,29 +1,53 @@
 import cv2
 import numpy as np
 import random
+
 import matplotlib.pyplot as plt
 
+# ==============this code added==================================================================:
+# import pydevd_pycharm
 
-def extract_lines(img, objects, kernel_size=5):
+# pydevd_pycharm.settrace('192.168.0.4', port=12345, stdoutToServer=True,
+#                         stderrToServer=True)
+
+
+# ================================================================================================
+
+
+def extract_lines(img_path, objects, kernel_size=5):
     """
     :param kernel_size:
     :param img:
     :return:
     """
-
-    # TODO: implement
-    return
-
-
-def extract_lines_by_color(img):
-    red_img = img.copy()
-    red_img[:, :, 0] = 0
-    red_img[:, :, 1] = 0
-    # cv2.imshow(red_img)
-    indices = np.argwhere((red_img != 0).all())
-    print(indices)
-    lines = apply_hough_trafo(red_img)
+    # img_path = "./../test/hydr0_hand.png"
+    img = cv2.imread(img_path)
+    img = flatten_colors(img)
+    lines = apply_hough_trafo(img)
+    # todo: implement
+    #  merge_lines()
+    clusters = cluster_lines(lines)
+    # plot_clusters(clusters)
     return lines
+
+
+def extract_components(img):
+    random.seed(12345)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, 1, 2)
+    hull_list = []
+    for i in range(len(contours)):
+        hull = cv2.convexHull(contours[i])
+        hull_list.append(hull)
+    # Draw contours + hull results
+    drawing = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    for i in range(len(contours)):
+        color = (random.randint(0, 256), random.randint(0, 256), random.randint(0, 256))
+        cv2.drawContours(drawing, contours, i, color)
+        cv2.drawContours(drawing, hull_list, i, color)
+    # Show in a window
+    cv2.imwrite("./../test/result.png", drawing)
 
 
 def apply_hough_trafo(img):
@@ -33,7 +57,7 @@ def apply_hough_trafo(img):
     edges = cv2.Canny(blur_gray, 50, 150)
 
     line_image = np.copy(img) * 0
-    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=5, minLineLength=15, maxLineGap=20)
+    lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=5, minLineLength=25, maxLineGap=30)
     for line in lines:
         for x1, y1, x2, y2 in line:
             # cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
@@ -44,6 +68,8 @@ def apply_hough_trafo(img):
 
 def cluster_lines(lines, threshold=20):
     clusters = {}
+    if lines is None:
+        return clusters
     for line in lines:
         for x1, y1, x2, y2 in line:
             p1 = np.array([x1, y1])
@@ -80,10 +106,49 @@ def plot_clusters(clusters):
         plt.show()
 
 
-def test():
-    img_path = "./../test/ex2a.png"
-    img = cv2.imread(img_path)
-    lines = extract_lines_by_color(img)
-    # apply_agglomerative_clustering(lines)
-    clusters = cluster_lines(lines)
-    plot_clusters(clusters)
+def flatten_colors(img):
+    bins = 8
+    hist_r = cv2.calcHist([img], [0], None, [bins], [0, 256])
+    hist_g = cv2.calcHist([img], [1], None, [bins], [0, 256])
+    hist_b = cv2.calcHist([img], [2], None, [bins], [0, 256])
+
+    hist_sum_sm = np.sum([hist_r[:, 0], hist_g[:, 0], hist_b[:, 0]], axis=0)
+
+    min_vals = np.r_[True, hist_sum_sm[1:] < hist_sum_sm[:-1]] & np.r_[hist_sum_sm[:-1] < hist_sum_sm[1:], True]
+    max_vals = np.r_[True, hist_sum_sm[1:] > hist_sum_sm[:-1]] & np.r_[hist_sum_sm[:-1] > hist_sum_sm[1:], True]
+
+    start = 0
+    val = 0
+    multiplicator = (256 / bins)
+    for i in range(bins):
+        if min_vals[i]:
+            img[(img > start * multiplicator - 1) & (img < i * multiplicator)] = val * multiplicator
+            start = i
+        elif max_vals[i]:
+            val = i
+    img[(img > start * multiplicator - 1) & (img < 256)] = val * multiplicator
+
+    # r_channel = img[:, :, 0]
+    # g_channel = img[:, :, 1]
+    # b_channel = img[:, :, 2]
+
+    # img[np.where((r_channel == g_channel) & (g_channel == b_channel))] = 0
+    # kernel = np.ones((5, 5), np.uint8)
+    # img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    # img = cv2.dilate(img, kernel, iterations=1)
+    #
+    # plt.imshow(img)
+    # plt.show()
+
+    # eliminate color channels which have no effect on the lines
+    img[:, :, 1] = 0
+    img[:, :, 2] = 0
+    img[np.where(img[:, :, 0] > 200)] = 0
+    img[np.where(img[:, :, 0] < 50)] = 0
+    img[np.where(img[:, :, 0] != 0)] = 255
+
+    kernel = np.ones((5, 5), np.uint8)
+    erosion = cv2.erode(img, kernel, iterations=1)
+    # plt.imshow(erosion)
+    # plt.show()
+    return erosion
